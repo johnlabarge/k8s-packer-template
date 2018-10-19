2f70134dd249383ed05d2f42c6fd51c6ca7267d8
John La Barge <johnlabarge@google.com>
2018-10-19 16:20:04 -0500

    Updates to build

--- phase2.sh
+++
@@ -1,7 +0,0 @@
-packerio build \
--var "project_id=cloudjlb-packer-tutorial" \
--var "source_image=acme-centos7--partial" \
--var "image_name=acme-centos7-complete" \
--var "image_zone=us-central1-b" \
--var  "private_key_file=provisioner" \
-*-from-partial.json
--- scripts/linux-guest-environment.sh
+++ scripts/linux-guest-environment.sh
@@ -2,7 +2,12 @@
 
 #######################################
 ## Linux Guest Environment
-# clear out yum metadata
+curl -O https://packages.cloud.google.com/yum/doc/yum-key.gpg
+curl -O https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+rpm --import yum-key.gpg --quiet
+rpm --import rpm-package-key.gpg --quiet
+rm yum-key.gpg
+rm rpm-package-key.gpg 
 OS_RELEASE_FILE="/etc/redhat-release"
 if [ ! -f $OS_RELEASE_FILE ]; then
    OS_RELEASE_FILE="/etc/centos-release"
@@ -14,7 +19,7 @@ name=Google Cloud Compute
 baseurl=https://packages.cloud.google.com/yum/repos/google-cloud-compute-el${DIST}-x86_64
 enabled=1
 gpgcheck=1
-repo_gpgcheck=0
+repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOM
